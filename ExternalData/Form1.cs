f4d685e0d687fec18c02100b93dc6c5b539233d9
vsdelgado17 <131558946+vsdelgado17@users.noreply.github.com>
2024-12-02 10:46:46 -0600

    Added Student Id and Name to Form1.cs

--- ExternalData/Form1.cs
+++ ExternalData/Form1.cs
@@ -59,7 +59,7 @@ namespace ExternalData
                     cmd.ExecuteNonQuery();
                 }
             }
-            MessageBox.Show("Added new student successfully!");
+            MessageBox.Show("Added " + student.Id + ": " + student.FirstName + " successfully!");
         }
     }
 }
